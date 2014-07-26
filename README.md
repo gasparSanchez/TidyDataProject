c5df6a1e26442c35ff22f3dbeef4f8925e1e8b35
gasparSanchez <gaspar.sm@gmail.com>
2014-07-27 00:36:12 +0200

    First version
    First version of the assigment

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 TidyDataProject
 ===============
+Introduction
+------------
+It is now possible to collect a large amount of data about personal movement using activity monitoring devices such as a Fitbit, Nike Fuelband, or Jawbone Up. These type of devices are part of the "quantified self" movement -- a group of enthusiasts who take measurements about themselves regularly to improve their health, to find patterns in their behavior, or because they are tech geeks. But these data remain under-utilized both because the raw data are hard to obtain and there is a lack of statistical methods and software for processing and interpreting the data.
 
-Repository for the project of the Coursera Getting and Cleaning Data course
+The purpose of this project is to demonstrate the ability of collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis.  
+ 
+On this repository you could find:
+
+1. A tidy data set call TidyDataSet.txt created using various source of data.
+2. A tidy data set call TidyDataSetFinal.txt based in the first one but with some processing. 
+3. A script, run_analysis.R, that generates both tidy data set: TidyDataSet.txt and TidyDataSetFinal.txt
+4. A code book that describes the variables, the data, and any transformations or work  performed to clean up the data called and CodeBook.md
+5. This file, README.md, that summarizes all the process and serves as a guide
+
+The data
+--------
+The data we're going to use represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained: 
+
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+Here are the data for the project: 
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+
+Requirements for the tidy data sets 
+-----------------------------------
+We were asked to create one R script called run_analysis.R that does the following: 
+
+* Merges the training and the test sets of the data for the project to create one data set.
+* Extracts only the measurements on the mean and standard deviation for each measurement. 
+* Uses descriptive activity names to name the activities in the data set
+* Appropriately labels the data set with descriptive variable names. 
+* Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+The script
+----------
+I have created a R script called run_analysis.R that can be run and executes all the steps demanded in the assignment. The code is commented in order to better understand how de tidy data is generated.
---
+++ run_analysis.R
@@ -0,0 +1,48 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# This is the script that generates the tady data set #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# ::Training data::
+# Load the file with the names of the varables
+dataNames <- read.table("~/Dropbox/Cursos/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset/features.txt", header=FALSE)
+# Pass the names of the variables as header of the data
+dataTrain <- read.table("~/Dropbox/Cursos/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset/train/X_train.txt", col.names=dataNames$V2, header=FALSE)
+# Add the subject index to the data
+dataTrain$subject <- as.matrix(read.table("~/Dropbox/Cursos/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset/train/subject_train.txt", header=FALSE))
+# Add the activity index to the data
+dataTrain$activity <- as.matrix(read.table("~/Dropbox/Cursos/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset/train/y_train.txt", header=FALSE))
+dataTrain <- as.data.frame(dataTrain)
+#
+# ::Test data::
+# Repeat the same steps with the test data
+dataTest <- read.table("~/Dropbox/Cursos/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset/test/X_test.txt", col.names=dataNames$V2, header=FALSE)
+dataTest$subject <- as.matrix(read.table("~/Dropbox/Cursos/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset/test/subject_test.txt", header=FALSE))
+dataTest$activity <- as.matrix(read.table("~/Dropbox/Cursos/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset/test/y_test.txt", header=FALSE))
+dataTest <- as.data.frame(dataTest)
+#
+# ::Binding Training and Test data::
+# I put together the two data sets: training and test
+data <- rbind.data.frame(dataTrain, dataTest)
+# Change the activity indices to activity labels according with the correspondence in activity_labels.txt
+activityLabels <- read.table("~/Dropbox/Cursos/Coursera/Getting and Cleaning Data/Project/UCI HAR Dataset/activity_labels.txt", header=FALSE)
+ActLabels <- as.matrix(activityLabels$V2)
+ActLabels[1:6]
+data$activity <- factor(data$activity, labels=ActLabels)
+# Selecting the valid columns for the tidy data set
+validCol <- c("subject", "activity", grep(".mean", colnames(data), value=TRUE), grep(".std", colnames(data), value=TRUE))
+# Redefining the data, this is my tidy data set
+data <- data[,validCol]
+# Creating the tidy data set
+setwd("~/Dropbox/Cursos/Coursera/Getting and Cleaning Data/Project/TidyDataProject/")
+write.table(data, file="TidyDataSet.txt")
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# Table with the average of each variable for each activity and each subject. #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+library("reshape2")
+# Using aggregate to generate the new table with 
+# the average of each variable for each activity and each subject.
+dataSummary <-aggregate(data[3:81], by=list(data$subject,data$activity),FUN=mean, na.rm=TRUE)
+names(dataSummary)[1]<-"subject"
+names(dataSummary)[2]<-"activity"
+write.table(dataSummary, file="TidyDataSetFinal.txt")
